f6c485ad76b850977814cc9e1562c36f36aae440
Ekaterina Voitkovich <styardess@mail.ru>
2022-11-09 15:28:19 +0300

    degree

--- Задача_25/Program.cs
+++ Задача_25/Program.cs
@@ -5,3 +5,25 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
+int GetDegree(int num, int degree)
+{
+    int Degree = 0;
+
+    for  (int i = 0; i < degree; i++)
+    {
+        Degree = num * degree;
+    }
+    return Degree;
+}
+
+
+
+
+Console.WriteLine("Введите первое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Первое число возведено в натуральную степень второго числа, результат равен: " 
++ GetDegree(num, degree));
